4d1c390aa4608354cd6ca832d37e4719d441ac13
Math2345 <maxim.hramtsoff@gmail.com>
2020-10-08 13:10:33 +0300

    finished calculate.js

---
+++ calculate.js
@@ -0,0 +1,69 @@
+function Calc() {
+    let self = this;
+
+    this._cache = [];
+
+    this._allOperation = [
+        {
+            sign: '+',
+            operation: (a, b) => a + b
+        },
+        {
+            sign: '*',
+            operation: (a, b) => a * b
+        }
+    ];
+
+
+    this.operation = (expr) =>  {
+        if (expr && typeof expr === 'string') {
+
+            const sign = expr.match(/[\+\-\*\/]/g)[0];
+            const [num1, num2] = expr.match(/\d+/g);
+            const funcOperation = this._allOperation.find((elem) => sign === elem.sign );
+
+            this._cache.push({
+                operation: sign, operands: [num1, num2]
+            });
+
+            return funcOperation.operation(+num1, +num2);
+        }
+        else {
+            return false;
+        }
+    }
+
+    this.addOperation = (sign, callback) => {
+        this._allOperation.push({
+            sign: sign,
+            operation: callback
+        })
+    }
+
+    this.history = () => {
+        return this._cache;
+    }
+
+    this.clearHistory = () => {
+        this._cache.splice(0, this._cache.length);
+    }
+
+    return {
+        operation: self.operation,
+        addOperation: self.addOperation,
+        history: self.history,
+        clearHistory: self.clearHistory
+    }
+}
+
+
+const calculator = new Calc()
+
+console.log(calculator.operation('31 + 32')) // 63
+console.log(calculator.operation('10 * 2')) // 10
+console.log(calculator.addOperation('/', (a, b) => a / b))
+console.log(calculator.operation('10 / 2')) // 5
+
+console.log(calculator.history());
+calculator.clearHistory();
+console.log(calculator.history());
